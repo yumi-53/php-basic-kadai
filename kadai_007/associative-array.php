c399058ee3759480d1c85b76615877577c951cb0
yumi-53 <yk53713@gmail.com>
2024-06-05 01:18:34 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+      <?php
+        $item_data = [
+          'name' => 'onion',
+          'price' => 200,
+          'weight' => 160
+        ];
+
+        print_r ($item_data);
+      ?>
+  </p>
+</body>
+
+</html>
