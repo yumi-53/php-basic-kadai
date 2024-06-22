e28272d2c4fff4475965830ce9eed4d56d37dd4b
yumi-53 <yk53713@gmail.com>
2024-06-22 11:49:10 +0900

    add:form,cookie,validation

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>登録完了</title>
+  <!-- <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css"> -->
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+  <script src="./js/script.js" defer></script>
+</head>
+<body>
+  <h2>登録が完了しました。</h2>
+  <p>社員情報がデータベースに保存されました。</p>
+  <button id="back_btn" onclick="location.href='form.php';">戻る</button>
+
+</body>
+</html>
