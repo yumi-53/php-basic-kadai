84bef823ce14eb277aed18b84362da55490ca9dd
yumi-53 <yk53713@gmail.com>
2024-06-26 00:50:22 +0900

    add:レイアウト調整

--- kadai_web_app/complete.php
+++ kadai_web_app/complete.php
@@ -5,14 +5,26 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>登録完了</title>
-  <!-- <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css"> -->
-  <link rel="stylesheet" type="text/css" href="./css/style.css">
-  <script src="./js/script.js" defer></script>
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=M+PLUS+1p&family=Playwrite+NG+Modern:wght@100..400&display=swap" rel="stylesheet">  <!-- <script src="./js/script.js" defer></script> -->
 </head>
 <body>
-  <h2>登録が完了しました。</h2>
-  <p>社員情報がデータベースに保存されました。</p>
-  <button id="back_btn" onclick="location.href='form.php';">戻る</button>
-
+  <main>
+    <h2>登録が完了しました。</h2>
+    <ul>
+        <li><div class="item-number">01.</div><div class="item-title">入力フォーム</div></li>
+        <li><div class="item-number">02.</div><div class="item-title">内容確認</div></li>
+        <li><div class="item-number">03.</div><div class="item-title current-page">登録完了</div></li>
+    </ul>
+    <div class="form-area">
+      <p>社員情報がデータベースに保存されました。</p>
+      <div class="btn-area">
+        <button id="back_btn" onclick="location.href='form.php';">戻る</button>
+      </div>
+    </div>
+  </main>
 </body>
 </html>
--- kadai_web_app/confirm.php
+++ kadai_web_app/confirm.php
@@ -28,9 +28,6 @@ if (empty($errors)) {
   setcookie('name', $name, time() + 3600);
   setcookie('age', $age, time() + 3600);
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -40,49 +37,60 @@ if (empty($errors)) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>確認画面</title>
-  <!-- <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css"> -->
-  <link rel="stylesheet" type="text/css" href="./css/style.css">
-  <script src="./js/script.js" defer></script>
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=M+PLUS+1p&family=Playwrite+NG+Modern:wght@100..400&display=swap" rel="stylesheet">  <!-- <script src="./js/script.js" defer></script> -->
 </head>
 <body>
-  <h2>入力画面をご確認ください。</h2>
-  <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
-  
-  <table>
-    <tr>
-      <th>項目</th>
-      <th>入力内容</th>
-    </tr>
-    <tr>
-      <td>社員名</td>
-      <td><?php echo $name; ?></td>
-    </tr>
-    <tr>
-      <td>年齢</td>
-      <td><?php echo $age; ?></td>
-    </tr>
-    <tr>
-      <td>所属部署</td>
-      <td><?php echo $department; ?></td>
-    </tr>
-  </table>
-  <?php
-    // エラー表示
-    if (!empty($errors)) {
-      foreach ($errors as $error) {
-        echo '<p class="error">' . $error . '</p>';
-      }
+  <main>
+    <h2>入力画面をご確認ください。</h2>
+    <ul>
+        <li><div class="item-number">01.</div><div class="item-title">入力フォーム</div></li>
+        <li><div class="item-number">02.</div><div class="item-title current-page">内容確認</div></li>
+        <li><div class="item-number">03.</div><div class="item-title">登録完了</div></li>
+    </ul>
+    <div class="form-area">
+      <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+      <?php
+        // エラー表示
+        if (!empty($errors)) {
+          foreach ($errors as $error) {
+            echo '<p class="error">' . $error . '</p>';
+          }
 
-      // 確定ボタン無効化
-      $disabled_btn = '<script> document.getElementById("confirm-btn").disabled = true; </script>';
-    } else {
-      $disabled_btn = "";
-    }
-  ?>
-  <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
-  <button id="cancel-btn" onclick="history.back();">キャンセル</button>
-  <?php echo $disabled_btn; ?>
-  
-
+          // 確定ボタン無効化
+          $disabled_btn = '<script> document.getElementById("confirm-btn").disabled = true; </script>';
+        } else {
+          $disabled_btn = "";
+        }
+      ?>
+      <table class="form-item">
+        
+        <tr>
+          <th>項目</th>
+          <td>入力内容</td>
+        </tr>
+        <tr>
+          <th>社員名</th>
+          <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+          <th>年齢</th>
+          <td><?php echo $age; ?></td>
+        </tr>
+        <tr>
+          <th>所属部署</th>
+          <td><?php echo $department; ?></td>
+        </tr>
+      </table>
+      <div class="btn-area">
+        <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+        <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+        <?php echo $disabled_btn; ?>
+      </div>
+    </div>
+  </main>
 </body>
 </html>
